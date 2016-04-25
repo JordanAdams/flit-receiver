012ceecfff6ecfa26155d708f2d537109b4973de
Jordan Adams <hi@jordanadams.co.uk>
2016-04-25 11:40:35 +0100

    Removes unused import

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,5 @@
 import path from 'path';
 import winston from 'winston';
-// import config from './config';
 
 winston.add(winston.transports.File, {
   filename: path.join(__dirname, '..', 'logs', 'application.log')
